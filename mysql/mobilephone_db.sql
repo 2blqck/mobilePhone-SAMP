829a2c939276628de87a15427312926c6a0b14a2
twoblqck <madou7311@gmail.com>
2025-03-24 23:21:37 +0100

    mySql update

--- mysql/mobilephone_db.sql
+++ mysql/mobilephone_db.sql
@@ -1,67 +1,41 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Aug 06, 2024 at 10:47 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.32-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.8.0.6908
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Database: `mobilephone_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `twitter`
---
-
-CREATE TABLE `twitter` (
+-- Dumping structure for table mobilephone_db.twitter
+CREATE TABLE IF NOT EXISTS `twitter` (
   `TweetID` int(2) DEFAULT 1,
   `TweetString` varchar(93) DEFAULT 'Prazno'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `twitter`
---
+-- Data exporting was unselected.
 
-INSERT INTO `twitter` (`TweetID`, `TweetString`) VALUES
-(3, 'proba_tri'),
-(1, 'proba_texta'),
-(2, 'Ovo_je_drugi_tweet_po~n~_redu_i_moze_stati_u_~n~vise_redova');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `Username` varchar(32) NOT NULL,
+-- Dumping structure for table mobilephone_db.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `Username` varchar(32) DEFAULT NULL,
   `HasPhone` tinyint(1) DEFAULT 0,
   `Number` int(8) DEFAULT NULL,
-  `Credit` int(8) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `Credit` int(8) DEFAULT NULL,
+  `Frame` int(2) DEFAULT 0,
+  `Background` int(2) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `users`
---
+-- Data exporting was unselected.
 
-INSERT INTO `users` (`Username`, `HasPhone`, `Number`, `Credit`) VALUES
-('Emo_Khalifa', 1, 232973, 120);
-COMMIT;
-
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
